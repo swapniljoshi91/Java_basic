a33e1e2439db9c11c50a4bb8913699b6ac5db15d
Swapnil <swapniljoshiind@gmail.com>
2025-12-08 16:18:47 +0530

    first java commit

---
+++ Operators/Oprators.java
@@ -0,0 +1,24 @@
+class Operators{
+	public static void main (String[]  areg)
+	{
+	// on this we are going to know about Addignment Operators 
+	
+	/* 
+	int  x=5 
+	
+	
+	*/
+	
+	int  x = 5;
+	int  a = a + 12;
+	int  b = b+  15;
+	
+
+
+	System.out.println( x );
+	
+	
+	}
+
+
+}
---
+++ my_first.java
@@ -0,0 +1,11 @@
+class my_first {
+
+	public static void main(String a[ ])
+	{
+	System.out.println("Hello i am working on it");
+	//System.out.println
+	
+	}
+
+
+}
