0f813c8c977c505a627101679049e7c925429799
Swapnil <swapniljoshiind@gmail.com>
2025-12-09 19:22:07 +0530

    var comment 3

---
+++ variable/javalearn.java
@@ -0,0 +1,25 @@
+public class javalearn {
+	public static void main(String[] agrn)
+	{
+		int a = 130;
+		byte b = 3;
+		float f = 234.44334433f;
+		double d = 4344.12232232222222333455434562222222;
+		
+		System.out.println(b);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println("----------------------");
+		System.out.println("a="+ a);
+		System.out.println("Pre increment ++a= "+ ++a);
+		System.out.println("after pre incrementa= "+ a);
+		System.out.println("a++ post increment= "+ a++);
+		System.out.println("after post increment a= "+ a);
+		System.out.println("Pre decriment --a= "+ --a);
+		System.out.println("ater pre decriment a= "+ a);
+		System.out.println("a-- post decriment= "+ a--);
+		System.out.println("after post decriment a= "+ a);
+		
+	}
+
+}
