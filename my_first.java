7803992edeace9bed0032e656e58e56663687446
Swapnil <swapniljoshiind@gmail.com>
2025-12-09 19:38:35 +0530

    testing 3

--- my_first.java
+++ my_first.java
@@ -3,7 +3,24 @@ class my_first {
 	public static void main(String a[ ])
 	{
 	System.out.println("Hello i am working on it");
-	//System.out.println
+	for (int i =1; i<5;i++){
+		
+		System.out.println(i);
+	}
+/* 		for (int i =1; i<5; ++i){
+		
+		System.out.print(i);
+	} */
+	
+	for (int i= 1; i <6; i ++){
+		
+		for (int j = i; j<6; j++)
+		{
+			
+			System.out.print(i);
+		}
+		System.out.println();
+	}
 	
 	}
 
