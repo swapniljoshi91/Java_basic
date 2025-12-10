566efa96c09177e0419b42118a52ae1ce34cfe14
Swapnil <swapniljoshiind@gmail.com>
2025-12-10 18:29:32 +0530

    fact given bp usre

---
+++ variable/userfact.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class userfact{
+
+	public static void main(String[] args){
+		int us_in;
+		int fact = 1;
+		System.out.print("Enter your No.: ");
+		Scanner u = new Scanner(System.in);
+		us_in = u.nextInt();
+		for (int i =1 ; i <= us_in; i++){
+			fact = fact * i;
+			
+			
+		}
+	System.out.println("Factor of "+ us_in + "  is  " + fact);
+	
+	}
+
+
+
+
+
+}
