47ac4c337efdb5f088f1573e7766dd1fe01fbc89
Swapnil <swapniljoshiind@gmail.com>
2025-12-10 18:07:06 +0530

    this is fact! quetion list part one 1

---
+++ variable/fect.java
@@ -0,0 +1,12 @@
+public class fect{
+// gitv me factore or 5!
+	public static void main(String[] arge){
+	int fact = 1;
+	for (int i = 1; i <=5; i++){
+		fact = fact*i;
+	}
+	System.out.println(fact);
+	
+	}
+
+}
