8d94e3a0140a6eb833877dad2b8efef5f0085b18
Swapnil <swapniljoshiind@gmail.com>
2025-12-11 18:43:05 +0530

    java table

---
+++ variable/table.java
@@ -0,0 +1,26 @@
+//tack input from user and print table of it 
+
+import java.util.Scanner;
+
+public class table{
+	public static void main(String [] args){
+		int us_ip;
+		System.out.println("Enter your No.: ");
+		Scanner s = new Scanner(System.in);
+		us_ip = s.nextInt();
+		for (int i =1; i <= 10; i++){
+				System.out.println(us_ip*i);
+				
+			
+			
+		}
+		
+		
+		
+	}
+
+
+
+
+
+}
